7afc9cd9470409e8e1c186c9db3d4d3d9f463fa5
emilianospinoso <emilianospinoso@gmail.com>
2022-04-13 14:23:20 -0300

    primera version

---
+++ script.js
@@ -0,0 +1,62 @@
+//Esto es un alert
+//alert("Hola Mundo")
+
+/*
+est
+es 
+comentario
+*/
+
+let alumna = "maria"
+
+alumna ="Claudia"
+
+let dni = 34025356 
+
+//console.log("La alumna se llama " + alumna + "y el dni es : " + dni)
+
+//definir varibales
+let nombreApellido = " Emiliano Spinoso"
+let nombre_apellido = "juana castro"  
+let NombreApellido = "otro nombre"
+let $nombreapellido = "alberto carlos"
+let _nombreapellido = "romina "
+let nombre1_apellido1
+let NombreA
+//malllllllllllllllllllllllllll
+/*let nombre-apellido = "ramiro lalal"
+let 1erNombre;
+let this;*/
+
+
+////////////tipos de datos
+//////////////////////Strings
+
+let str1 = "string 1 "
+let str2 = 'string 2'
+let str3 = str1 + "   "+ str2
+let str4 = `${str1}  y ademas me gusta el     ${str2}`
+
+
+/////////NUMBER
+//let nombreNuevo=prompt("Como te llamas") //maria
+//console.log("Se llama "+nombreNuevo)
+let numero1 = 0
+
+numero1 = parseFloat( prompt("mete el primer numero"))  
+let numero2 =   parseFloat(  prompt("mete el segundo numero"))
+
+//calculadora
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multi = numero1 * numero2
+let divi = numero1 / numero2
+
+alert("La suma es "  + suma + "\n" + "La resta es "  + resta + "\n " +"La multiplicacion es "  + multi )
+
+
+
+
+
+
+
